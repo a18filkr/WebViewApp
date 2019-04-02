f4583b68012ff20b24123499acf5698f87697c76
Filip <a18filkr@student.his.se>
2019-04-02 09:29:22 +0200

    COMMMMMMMMMIT igen

--- app/src/main/java/com/example/brom/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/brom/webviewapp/MainActivity.java
@@ -40,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         // -- Commit and push to your github fork
         // 5. Enter the url to load in our WebView
         // -- Commit and push to your github fork
+        // Kommenterar lite för att se om det funkar att committa
     }
 
     @Override
